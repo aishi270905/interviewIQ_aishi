01303bea01e5b1bf1117e3deade8d3a7616aa791
Aishi <aishipandeyofficial@gmail.com>
2026-08-03 23:31:39 +0530

    interview history page modification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
     <Route path = '/auth' element = {<Auth/>}/>
     <Route path = '/interview' element = {<InterviewPage/>}/>
     <Route path = '/history' element = {<InterviewHistory/>}/>
-    <Route path = '/report' element = {<InterviewReport/>}/>
+    <Route path = '/report/:id' element = {<InterviewReport/>}/>
     <Route path = '/pricing' element = {<Pricing/>}/>
    </Routes>
   )
